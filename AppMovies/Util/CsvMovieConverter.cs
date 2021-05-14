055c5cd3f8172d950c82120e79f0c4bcd68543ac
RobertoRomolini <romolini.roberto@gmail.com>
2021-05-14 10:03:22 +0200

    Completed converter csv to movies list

---
+++ AppMovies/Util/CsvMovieConverter.cs
@@ -0,0 +1,39 @@
+﻿using AppMovies.Entities;
+using AppMovies.Models;
+using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace AppMovies.Util
+{
+    public class CsvMovieConverter : ICsvMovieConverter
+    {
+        public List<MovieEntity> GetMoviesFromCsv (Stream csvBlob)
+        {
+            List<MovieEntity> movies = new List<MovieEntity>();
+
+            var streamReader = new StreamReader(csvBlob);
+            var csv = new CsvReader(streamReader , System.Globalization.CultureInfo.CurrentCulture);
+                
+            if (csv.Read())
+            {   
+                csv.ReadHeader();
+                while (csv.Read())
+                {
+                    var movieEntity = new MovieEntity(csv.GetField("id"));
+                    movieEntity.Title = csv.GetField("title");
+                    movieEntity.Director = csv.GetField("director");
+                    movieEntity.Genre = csv.GetField("genre");
+                    movieEntity.Year = Int32.Parse( csv.GetField("year"));
+                    movieEntity.Country = csv.GetField("country");
+                    movieEntity.ImageUrl = csv.GetField("imageurl");
+                    movies.Add(movieEntity);
+                }
+            }
+
+            return movies;
+        }
+    }
+}
---
+++ AppMovies/Util/ICsvMovieConverter.cs
@@ -0,0 +1,14 @@
+﻿using AppMovies.Entities;
+using AppMovies.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace AppMovies.Util
+{
+    public interface ICsvMovieConverter
+    {
+        List<MovieEntity> GetMoviesFromCsv(Stream csvBlob);
+    }
+}
